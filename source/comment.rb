ac090f8aac474901fe1138ed600d7f43b4d06e27
zouyaoji <370681295@qq.com>
2018-03-09 23:18:30 +0800

    更新

---
+++ source/comment.rb
@@ -0,0 +1,32 @@
+username = "zouyaoji" # GitHub 用户名
+new_token = "ARGV.first"  # GitHub Token
+repo_name = "gitalkBlog" # 存放 issues
+sitemap_url = "https://zouyaoji.top/sitemap.xml" # sitemap
+kind = "Gitalk" # "Gitalk" or "gitment"
+
+require 'open-uri'
+require 'faraday'
+require 'active_support'
+require 'active_support/core_ext'
+require 'sitemap-parser'
+require 'openssl'
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL:VERIFY_NONE
+puts "Token: #{new_token}"
+
+sitemap = SitemapParser.new sitemap_url
+urls = sitemap.to_a
+
+conn = Faraday.new(:url => "https://api.github.com/repos/#{username}/#{repo_name}/issues") do |conn|
+  conn.basic_auth(username, token)
+  conn.adapter  Faraday.default_adapter
+end
+
+urls.each_with_index do |url, index|
+  title = open(url).read.scan(/<title>(.*?)<\/title>/).first.first.force_encoding('UTF-8')
+  response = conn.post do |req|
+    req.body = { body: url, labels: [kind, url], title: title }.to_json
+  end
+  puts response.body
+  sleep 15 if index % 20 == 0
+end
